cd9ed6ac12d932eef9e11c88b9a3189f7f3cb0e9
ZorbaHerbalist <tomek.trojanowski@gmail.com>
2025-07-21 13:59:40 +0200

    Initial commit

---
+++ src/main/java/info/qbnet/jtvdemo/DemoApp.java
@@ -0,0 +1,13 @@
+package info.qbnet.jtvdemo;
+
+import info.qbnet.jtvision.TApplication;
+
+public class DemoApp extends TApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        DemoApp app = new DemoApp();
+    }
+}
+
---
+++ src/main/java/info/qbnet/jtvision/TApplication.java
@@ -0,0 +1,8 @@
+package info.qbnet.jtvision;
+
+public class TApplication {
+
+    public TApplication() {
+        System.out.println("Start");
+    }
+}
