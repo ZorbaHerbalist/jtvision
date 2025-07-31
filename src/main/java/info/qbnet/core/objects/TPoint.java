ea1eca08e51049710ecaad8058bab35c74087765
ZorbaHerbalist <tomek.trojanowski@gmail.com>
2025-07-31 09:53:23 +0200

    Added TPoint class

---
+++ src/main/java/info/qbnet/core/objects/TPoint.java
@@ -0,0 +1,16 @@
+package info.qbnet.core.objects;
+
+public class TPoint {
+
+    public int x;
+    public int y;
+
+    public TPoint(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public TPoint() {
+        this(0, 0);
+    }
+}
