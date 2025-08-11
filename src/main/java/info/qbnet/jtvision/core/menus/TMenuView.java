b57563757d39c964b0ca8800c28d71ac44458cfc
ZorbaHerbalist <tomek.trojanowski@gmail.com>
2025-08-11 21:48:17 +0200

    Started implementing menu handling

--- src/main/java/info/qbnet/jtvision/core/menus/TMenuView.java
+++ src/main/java/info/qbnet/jtvision/core/menus/TMenuView.java
@@ -1,21 +1,37 @@
 package info.qbnet.jtvision.core.menus;
 
+import info.qbnet.jtvision.core.event.TEvent;
 import info.qbnet.jtvision.core.objects.TRect;
 import info.qbnet.jtvision.core.views.TPalette;
 import info.qbnet.jtvision.core.views.TView;
 
 public class TMenuView extends TView {
 
-    protected TMenu menu;
+    protected TMenuView parentMenu = null;
+    protected TMenu menu = null;
     protected TMenuItem current = null;
 
     public static final TPalette C_MENU_VIEW = new TPalette(TPalette.parseHexString("\\x02\\x03\\x04\\x05\\x06\\x07"));
 
     public TMenuView(TRect bounds) {
         super(bounds);
+        eventMask |= TEvent.EV_BROADCAST;
 
-        // TODO
         logger.debug("{} TMenuView@TMenuView(bounds={})", getLogName(), bounds);
+    }
+
+    @Override
+    public int getHelpCtx() {
+        TMenuView c = this;
+        while (c != null && ((c.current == null) ||
+                (c.current.helpCtx() == HelpContext.HC_NO_CONTEXT) || (c.current.name() == null))) {
+            c = c.parentMenu;
+        }
+        if (c != null) {
+            return c.current.helpCtx();
+        } else {
+            return HelpContext.HC_NO_CONTEXT;
+        }
     }
 
     @Override
