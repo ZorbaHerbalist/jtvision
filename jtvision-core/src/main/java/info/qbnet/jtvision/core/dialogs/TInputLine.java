a48af5eef262d147deb6a2b2d785f9074e0a9019
ZorbaHerbalist <tomek.trojanowski@gmail.com>
2025-08-29 16:28:58 +0200

    Implemented draw() method

--- jtvision-core/src/main/java/info/qbnet/jtvision/core/dialogs/TInputLine.java
+++ jtvision-core/src/main/java/info/qbnet/jtvision/core/dialogs/TInputLine.java
@@ -1,6 +1,7 @@
 package info.qbnet.jtvision.core.dialogs;
 
 import info.qbnet.jtvision.core.objects.TRect;
+import info.qbnet.jtvision.core.views.TDrawBuffer;
 import info.qbnet.jtvision.core.views.TPalette;
 import info.qbnet.jtvision.core.views.TView;
 import java.nio.ByteBuffer;
@@ -10,6 +11,12 @@ public class TInputLine extends TView {
 
     protected int maxLen;
     protected StringBuilder data;
+
+    protected int firstPos = 0;
+    protected int curPos = 0;
+
+    protected int selStart = 0;
+    protected int selEnd = 0;
 
     public static final  TPalette C_INPUT_LINE = new TPalette(TPalette.parseHexString("\\x13\\x13\\x14\\x15"));
 
@@ -21,9 +28,49 @@ public class TInputLine extends TView {
         this.data = new StringBuilder(maxLen);
     }
 
+    /** Returns whether the line can be scrolled further by {@code delta}. */
+    private boolean canScroll(int delta) {
+        if (delta < 0) {
+            return firstPos > 0;
+        } else if (delta > 0) {
+            return data.length() - firstPos + 2 > size.x;
+        } else {
+            return false;
+        }
+    }
+
     @Override
     public int dataSize() {
         return data.toString().getBytes(StandardCharsets.UTF_8).length;
+    }
+
+    @Override
+    public void draw() {
+        TDrawBuffer buf = new TDrawBuffer();
+        short color = (state & State.SF_FOCUSED) != 0 ? getColor((short) 2)
+                : getColor((short) 1);
+
+        buf.moveChar(0, ' ', color, size.x);
+        int len = Math.min(size.x - 2, data.length() - firstPos);
+        if (len > 0) {
+            buf.moveStr(1, data.substring(firstPos, firstPos + len), color);
+        }
+        if (canScroll(1)) {
+            buf.moveChar(size.x - 1, (char) 0x10, getColor((short) 4), 1);
+        }
+        if ((state & State.SF_FOCUSED) != 0) {
+            if (canScroll(-1)) {
+                buf.moveChar(0, (char) 0x11, getColor((short) 4), 1);
+            }
+            int l = selStart - firstPos;
+            int r = selEnd - firstPos;
+            if (l < 0) l = 0;
+            if (r > size.x - 2) r = size.x - 2;
+            if (l < r) {
+                buf.moveChar(l + 1, (char) 0x00, getColor((short) 3), r - l);
+            }
+        }
+        writeLine(0, 0, size.x, size.y, buf.buffer);
     }
 
     @Override
